0e2b5db49f4c72599fd490d0e808b88a96d67c50
Ghanshyam Mishra <ghanshyammishra3205615@gmail.com>
2023-02-07 13:31:53 +0530

    push notification enabled

--- app/src/main/java/com/example/chatapp/Constants.java
+++ app/src/main/java/com/example/chatapp/Constants.java
@@ -1,5 +1,7 @@
 package com.example.chatapp;
 
+import java.util.HashMap;
+
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_NAME = "name";
@@ -23,6 +25,27 @@ public class Constants {
     public static final String KEY_RECEIVER_IMAGE = "receiverImage";
     public static final String KEY_LAST_MESSAGE = "lastMessage";
     public static final String KEY_AVAILABILITY = "availability";
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> remoteMsgHeaders = null;
+
+    public static HashMap<String, String> getRemoteMsgHeaders() {
+        if (remoteMsgHeaders == null) {
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAA9dc97Mg:APA91bFxuMSbjMBQeWKzQTWlBBQID5qdfjDJn0YafRqy5liWEtbgnwrcMBFjtv4ac_u0lbImflI7VgwYMEANnwkfxISnnDHVNSerwmBHON-oO8ds2GHP2jpIv8-YL3x1izHNP_u8pm9N"
+            );
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_CONTENT_TYPE,
+                    "application/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
 
 
